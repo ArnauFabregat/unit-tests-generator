ad118d6f8b1b896c3b0f70375ea03f2f717c21e3
ArnauFabregat <arnaufara89@gmail.com>
2026-03-12 15:37:54 +0100

    notebooks update

--- src/raggraph/walker.py
+++ src/raggraph/walker.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 import networkx as nx
 
-from src.raggraph.parserraggraph.parser import CodeGraphBuilder1, CodeGraphBuilder2
+from src.raggraph.parser import CodeGraphBuilder1, CodeGraphBuilder2
 
 # ------------------------------------------------------------
 # Directory walker
@@ -23,14 +23,14 @@ def iter_python_files(path, skip_init=True):
                 yield Path(root, f).as_posix()
 
 
-def build_graph_from_directory(path: str, skip_init: bool = True, save_graph: bool = False):
+def build_graph_from_directory(code_path: str, skip_init: bool = True, save_graph: bool = False, save_path: str = ""):
     graph = nx.DiGraph()
-    for file_path in iter_python_files(path, skip_init=skip_init):
+    for file_path in iter_python_files(code_path, skip_init=skip_init):
         tree = ast.parse(open(file_path, "r", encoding="utf-8").read())
         builder = CodeGraphBuilder1(file_path, graph)
         builder.visit(tree)
 
-    for file_path in iter_python_files(path, skip_init=skip_init):
+    for file_path in iter_python_files(code_path, skip_init=skip_init):
         tree = ast.parse(open(file_path, "r", encoding="utf-8").read())
         builder = CodeGraphBuilder2(file_path, graph)
         builder.attach_parents(tree)
@@ -38,6 +38,6 @@ def build_graph_from_directory(path: str, skip_init: bool = True, save_graph: bo
 
     if save_graph:
         # To open with Gephi in browser
-        nx.write_graphml(graph, "repo_graph.graphml")
+        nx.write_graphml(graph, save_path)
 
     return graph
